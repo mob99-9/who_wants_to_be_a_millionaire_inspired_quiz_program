617f9087f1a7f9eb6ab6d0334df8138b5aa7bf25
mob99-9 <jamesorozco20050@gmail.com>
2025-05-02 12:33:21 +0800

    realized its hard to make a table on txt file so i gave up before studying csv file handling

--- mechanics.py
+++ mechanics.py
@@ -1,17 +1,18 @@
 import random
+
 print("Who Wants to be a Millionaire")
 def main():
-    user_input = input("A. Start or B. Exit")
+    user_input = input("A. Start or B. Exit: ")
     if user_input == "A":
-        start()
+        game()
     elif user_input == "B":
         exit
 
-def start():
+def game():
     with open("questions.txt") as file:
-        questions = file.readline()
-        print(questions)
-
+        questions = file.readlines()
+        question = random.choice(questions)
+        print(list(question))
 
 
 if __name__ == "__main__":
--- question_maker.py
+++ question_maker.py
@@ -1,5 +1,3 @@
-# make a text file with questions and possible answers from the user
-
 # make a text file 
 running = True
 file_name = "questions.txt"
@@ -7,25 +5,26 @@ file_name = "questions.txt"
 # make a function for adding questions and answers to the text file
 
 def add_to_file(user_input):
-    with open(file_name, "a") as text_file:
-        text_file.write(f"{user_input}")
+    with open(file_name, "w") as text_file:
+        text_file.write(user_input)
 
 # ask user for a question, 4 possible answer (a, b, c, d) and the right answer (A), infinitely until saying exit
 
-choices = ["a", "b", "c", "d"]
+question = []
 
 while running == True:
     user_question = input("Enter a question (type 'exit' to exit): ")
     if user_question == "exit":
         running = False
     else:
-        add_to_file(f'\n{user_question.capitalize()} ')
+        question.append(user_question)
 
-        for letter in choices:
-            user_choices = input(f"{letter}. Enter a possible answer: ")
-            add_to_file(f"{letter}. {user_choices} ")
+        for letter in range(0,4):
+            user_choices = input("Enter a possible answer: ")
+            question.append(user_choices)
 
         user_answer = input("Enter the letter of the right answer: ")
-        add_to_file(f"Answer: {user_answer.capitalize()}")
+        question.append(user_answer)
+        add_to_file(str(question))
 
-# save the text file (It is auto save and auto reset every program run)
+# save the text file (It is auto save and auto reset every program run
