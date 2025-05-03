d30f6226e91fd4d3321085da349bc4e5a760c80d
mob99-9 <jamesorozco20050@gmail.com>
2025-05-04 01:57:58 +0800

    fixed mechanics

--- mechanics.py
+++ mechanics.py
@@ -1,4 +1,5 @@
 import random
+import csv
 
 print("Who Wants to be a Millionaire")
 def main():
@@ -9,10 +10,16 @@ def main():
         exit
 
 def game():
-    with open("questions.txt") as file:
-        questions = file.readlines()
-        question = random.choice(questions)
-        print(list(question))
+    with open("data_questions.csv", "r") as file:
+        reader = csv.reader(file)
+        question_choice = random.choice(list(reader))
+        quiz = question_choice[0]
+        choice_a = f"A. {question_choice[1]}"
+        choice_b = f"B. {question_choice[2]}"
+        choice_c = f"C. {question_choice[3]}"
+        choice_d = f"D. {question_choice[4]}"
+        correct_answer = question_choice[5]
+        print(quiz, choice_a, choice_b, choice_c, choice_d)
 
 
 if __name__ == "__main__":
