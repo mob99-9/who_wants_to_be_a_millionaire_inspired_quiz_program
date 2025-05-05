314bea3d5f30fa65168b9ff36637e374d81b6604
mob99-9 <jamesorozco20050@gmail.com>
2025-05-05 16:43:43 +0800

    finished beta app, might continue in summer vacation

--- beta_app.py
+++ beta_app.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import time
 import csv
 import random
 
@@ -12,8 +13,45 @@ main_window.iconphoto(True, logo)
 main_window.config(background="black")
 question = []
 
+def select_answer(user_answer):
+    global answer
+    answer = ""
+    answer = user_answer
+
+def restart_game():
+    restart_button.destroy()
+    exit_button.destroy()
+    submit_button.destroy()
+    main_window.config(bg="black")
+    game()
+
+def submit():
+    global restart_button, exit_button
+    if correct_answer == answer:
+        quiz_label.destroy()
+        letter_a.destroy()
+        letter_b.destroy()
+        letter_c.destroy()
+        letter_d.destroy()
+        main_window.config(bg="black")
+        game()
+    else:
+        main_window.config(bg="red")
+        quiz_label.destroy()
+        letter_a.destroy()
+        letter_b.destroy()
+        letter_c.destroy()
+        letter_d.destroy()
+        submit_button.destroy()
+        restart_button = Button(main_window, text="Restart", command=restart_game)
+        restart_button.place(x=340, y=300)
+        exit_button = Button(main_window, text="Exit", command=exit)
+        exit_button.place(x=340, y=330)
+
 def game():
-    global quiz, choice_a, choice_b, choice_c, choice_d, correct_answer
+    start_button.destroy()
+    exit_button.destroy()
+    global quiz, choice_a, choice_b, choice_c, choice_d, correct_answer, submit_button, quiz_label, letter_a,letter_b,letter_c,letter_d
     with open("data_questions.csv", "r") as file:
         reader = csv.reader(file)
         question_choice = random.choice(list(reader))
@@ -28,18 +66,24 @@ def game():
                    font=("Times New Roman", 25))
     quiz_label.pack()
 
-    letter_a = Button(main_window, text=f"{choice_a}")
-    letter_a.place(x=100, y=400)
+    letter_a = Button(main_window, text=f"{choice_a}", command=lambda: select_answer("A"),
+                      activebackground="black")
+    letter_a.place(x=100, y=200)
 
-    letter_b = Button(main_window, text=f"{choice_b}")
-    letter_b.place(x=100, y=500)
+    letter_b = Button(main_window, text=f"{choice_b}", command=lambda: select_answer("B"),
+                      activebackground="black")
+    letter_b.place(x=100, y=300)
 
-    letter_c = Button(main_window, text=f"{choice_c}")
-    letter_c.place(x=400, y=400)
+    letter_c = Button(main_window, text=f"{choice_c}", command=lambda: select_answer("C"),
+                      activebackground="black")
+    letter_c.place(x=500, y=200)
 
-    letter_d = Button(main_window, text=f"{choice_d}")
-    letter_d.place(x=400, y=500)
+    letter_d = Button(main_window, text=f"{choice_d}", command=lambda: select_answer("D"),
+                      activebackground="black")
+    letter_d.place(x=500, y=300)
 
+    submit_button = Button(main_window, text="Submit", command=submit)
+    submit_button.place(x=340,y=340)
 
 title = Label(main_window, text="Who Wants To Be A Millionaire", 
               font=("Times New Roman", 40),
@@ -49,5 +93,7 @@ title.pack()
 start_button = Button(main_window, text="Start", command=game)
 start_button.place(x=340, y=300)
 
+exit_button = Button(main_window, text="Exit", command=exit)
+exit_button.place(x=340, y=330)
 
 main_window.mainloop()
--- mechanics.py
+++ mechanics.py
@@ -10,17 +10,18 @@ def main():
         exit
 
 def game():
+    global quiz, choice_a, choice_b, choice_c, choice_d, correct_answer
     with open("data_questions.csv", "r") as file:
         reader = csv.reader(file)
         question_choice = random.choice(list(reader))
         quiz = question_choice[0]
-        choice_a = f"A. {question_choice[1]}"
-        choice_b = f"B. {question_choice[2]}"
-        choice_c = f"C. {question_choice[3]}"
-        choice_d = f"D. {question_choice[4]}"
+        choice_a = question_choice[1]
+        choice_b = question_choice[2]
+        choice_c = question_choice[3]
+        choice_d = question_choice[4]
         correct_answer = question_choice[5]
-        print(quiz, choice_a, choice_b, choice_c, choice_d)
 
-
-if __name__ == "__main__":
-    main()
+if "B" == "B":
+    print("true")
+else:
+    print("false")
